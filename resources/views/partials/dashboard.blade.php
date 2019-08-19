612a3e54901eacccb7a6e1d7741e1effb594d5e0
Tohfa Akib <tohfaakib@my.smccd.edu>
2019-08-19 20:59:26 +0600

    ...

--- resources/views/partials/dashboard.blade.php
+++ resources/views/partials/dashboard.blade.php
@@ -49,7 +49,7 @@
                                 </li>
                                 @endif
 
-                                @if($user->user_type == 'member' && $user->status == 'invited')
+                                @if($user->status == 'invited')
                                     <li>
                                         <a href="/mess/invitation/{{session('mess_id')}}" class="view btn-sm active">Mess Invitation</a>
                                     </li>
