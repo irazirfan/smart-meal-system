0a4a771f0181460dc092254686d6e3e20c59789a
Tohfa Akib <tohfaakib@my.smccd.edu>
2019-08-20 12:32:28 +0600

    meal calculation

--- app/Http/Controllers/MealCalccontroller.php
+++ app/Http/Controllers/MealCalccontroller.php
@@ -37,22 +37,30 @@ class MealCalccontroller extends Controller
                 }
             }
         }
-//        dd($e_users);
 
         $total_amount = array_sum($e_users);
-        dd($total_amount);
-//        dd($meals);
+
+
 
         $total_meal_user_wise = [];
+        $total_meal = 0;
 
         for ($i=0; $i<count($meals); $i++){
             $total_user_meal = 0;
             $total_user_meal = $total_user_meal + $meals[$i]->breakfast + $meals[$i]->lunch + $meals[$i]->dinner;
+            $total_meal = $total_meal + $total_user_meal;
             array_push($total_meal_user_wise, $total_user_meal);
         }
 //        dd($total_meal_user_wise);
 
+//        $total_meal = array_sum($total_meal_user_wise);
 
-        return view('calculation/calculation', ['user'=>$user, 'name_list'=>$names, "meals"=>$meals, "total_user_meal"=>$total_user_meal,"total_meal_user_wise"=>$total_meal_user_wise, 'amounts'=>$e_users]);
+        $meal_rate = $total_amount / $total_meal;
+
+
+
+
+        return view('calculation/calculation', ['user'=>$user, 'name_list'=>$names, "meals"=>$meals,
+            "total_meal"=>$total_meal,"total_meal_user_wise"=>$total_meal_user_wise, 'amounts'=>$e_users, 'meal_rate'=>$meal_rate]);
     }
 }
--- resources/views/calculation/calculation.blade.php
+++ resources/views/calculation/calculation.blade.php
@@ -23,67 +23,28 @@
         <tbody>
 
 
-
-
-
-{{--        <input type="hidden" <%= user_amount = []; %> >--}}
-
-<!--        <% for(var i=0; i < e_name_list.length; i++) { %>-->
-<!--            <input type="hidden" <%= single_amount = 0; %> >-->
-<!--            <% for(var j=0; j < e_name_list[i].length; j++) { %>-->
-<!--                <input type="hidden" <%= single_amount = single_amount + e_name_list[i][j].amount %> >-->
-<!---->
-<!--            <% } %>-->
-<!--            <input type="hidden" <%= user_amount.push(single_amount); %> >-->
-<!--        <% } %>-->
-<!---->
-<!--        <input type="hidden" <%= total_amount = 0; %> >-->
-<!---->
-<!--        <% for(var i=0; i < user_amount.length; i++) { %>-->
-<!---->
-<!--            <input type="hidden" <%= total_amount = total_amount + user_amount[i]; %>>-->
-<!--        <% } %>-->
-
-
-<!--        <input type="hidden" <%= total_meal_all_user = 0; %> >-->
-
-<!--        <% for(var i=0; i < total_meal.length; i++) { %>-->
-<!--            <input type="hidden" <%= total_meal_all_user = total_meal_all_user + total_meal[i]; %> >-->
-<!--        <% } %>-->
-
-<!--        <input type="hidden" <%= meal_rate = total_amount / total_meal_all_user; %> >-->
-
-
-            @for(var i=0; i < count(name_list); i++)
-                <tr>
-                    <td><%= name_list[i][0].name %></td>
-                    <td><%= total_meal[i] %></td>
-                    <td><%= user_amount[i] %></td>
-                    <td><%= (total_meal[i] * meal_rate).toFixed(2) %></td>
-                    <td><%= (user_amount[i] - (total_meal[i] * meal_rate)).toFixed(2) %></td>
+        @for($i=0; $i < count($name_list); $i++)
+            <tr>
+                <td>{{$name_list[$i]->name}}</td>
+                    <td>{{$total_meal_user_wise[$i]}}</td>
+                    <td>{{$amounts[$i]}}</td>
+                    <td>{{$total_meal_user_wise[$i] * $meal_rate }}</td>
+                    <td>{{ ($amounts[$i] - ($total_meal_user_wise[$i] * $meal_rate)) }}</td>
                 </tr>
             @endfor
 
-        <tr>
-            <th scope="row">Total</th>
-            <td><%= total_meal_all_user %></td>
-            <td><%= total_amount %></td>
-            <td><%= (meal_rate * total_meal_all_user).toFixed(2) %></td>
-            <td><%= ((meal_rate * total_meal_all_user)-total_amount).toFixed(2) %></td>
-        </tr>
+
+
         </tbody>
 
     </table>
 
 
-    <% if(!meal_rate){ %>
-        <p style="color: red">*Your mess' calculation has not bean started yet!</p>
-    <% } %>
 
 
     <div class="jumbotron">
 
-        <p style="font-weight: bolder">Meal Rate: <%= meal_rate.toFixed(2) %></p>
+        <p style="font-weight: bolder">Meal Rate: {{ $meal_rate }}</p>
 
     </div>
 
